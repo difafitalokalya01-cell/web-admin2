787acaa112092b44dc2942ebb41db5981639e75c
yusri ganteng <yusri338458@gmail.com>
2025-10-06 12:47:18 +0700

    defef

---
+++ src/app/dashboard/products/page.js
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'product page',
+    descriptions: 'mengelola produk'
+};
+
+export default function ProductPage() {
+    return (
+        <h1>this product</h1>
+    )
+}
