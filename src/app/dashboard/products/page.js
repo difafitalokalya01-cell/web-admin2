351b93bb5e8c9886963f954d38160a8f080f5599
yusri ganteng <yusri338458@gmail.com>
2025-11-24 20:25:05 +0700

    dawfa

--- src/app/dashboard/products/page.js
+++ src/app/dashboard/products/page.js
@@ -3,8 +3,6 @@ import ContentProductPage from "./components/content";
 import axios from "@/app/lib/axios";
 import { cookies } from "next/headers";
 
-
-
 export default async function ProductPages() {
   const cookieStore = await cookies(); 
   const token = cookieStore.get("admin_token")?.value;
