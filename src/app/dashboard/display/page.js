03f49e6146676f442771c09b2720fd57b3409719
Muhammad Yusri <yusri5865@gmail.com>
2026-02-08 17:31:43 +0700

    awgf

--- src/app/dashboard/display/page.js
+++ src/app/dashboard/display/page.js
@@ -3,7 +3,7 @@ import ContentDisplayPage from "./component/content";
 import { cookies } from "next/headers";
 
 export default async function ContactPages() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const token = cookieStore.get("admin_token")?.value;
 
   return (
